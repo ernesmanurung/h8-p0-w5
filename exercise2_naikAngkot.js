8aaff7e2e0f1beaaec0555e9826271de76472283
ernest <ernesmanurungg@gmail.com>
2018-10-17 13:39:23 +0700

    exercise

---
+++ exercise2_naikAngkot.js
@@ -0,0 +1,23 @@
+function naikAngkot(arrPenumpang) {
+    rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+    var output = [];
+    ongkos = 2000;
+
+  
+    for(var i = 0; i < arrPenumpang.length; i++){
+        var obj = {};
+        obj.penumpang = arrPenumpang[i][0];
+        obj.naikDari = arrPenumpang[i][1];
+        obj.tujuan = arrPenumpang[i][2];
+        obj.bayar = (rute.indexOf(arrPenumpang[i][2] - rute.indexOf(arrPenumpang[a][1]) * ongkos));
+        output.push(obj);
+    }
+    return output;
+  }
+  
+  //TEST CASE
+  console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
+  // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+  //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+  
+  console.log(naikAngkot([])); //[]
